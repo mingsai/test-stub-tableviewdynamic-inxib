adb54bdd77fa6b73b46ad6a7a7110d3671662f2c
Tommie N. Carter, Jr. MBA <tcarternyc@gmail.com>
2018-04-03 13:03:26 -0400

    Initial Commit

---
+++ test-stub-tableviewdynamic-inxib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-stub-tableviewdynamic-inxib
+//
+//  Created by Tommie N. Carter, Jr., MBA on 4/3/18.
+//  Copyright © 2018 Your Org Name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
